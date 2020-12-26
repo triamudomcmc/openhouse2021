0f283f053d5f0c5635198ad71c97cb320ebfa57c
Possawat Suksai <markkung08@gmail.com>
2020-12-26 21:53:31 +0700

    Restructure code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import Timer from '../utilities/timer'
+import Timer from '../components/Timer'
 
 const Index = () => (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-        <div className="text-6xl font-bold font-display">
-            TRIAM UDOM OPENHOUSE 2021
-        </div>
-        <div className="mt-4 text-6xl font-display">เตรียมอุดมโอเพ่นเฮาส์ ๒๐๒๑</div>
-        <Timer/>
+  <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="text-6xl font-bold font-display">
+      TRIAM UDOM OPENHOUSE 2021
     </div>
+    <div className="mt-4 text-6xl font-display">เตรียมอุดมโอเพ่นเฮาส์ ๒๐๒๑</div>
+    <Timer />
+  </div>
 )
 
 export default Index
