3689e5e9637989a0d91d4359521e09834df27965
Peter <peterphongpak@gmail.com>
2020-12-27 16:33:57 +0700

    Add Ouwae component

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,28 +2,28 @@ import React from 'react'
 import Router from 'next/router'
 
 const Timer = () => {
-  const [count, setCount] = React.useState(10)
+    const [count, setCount] = React.useState(10)
 
-  React.useEffect(() => {
-    if (count > 0) {
-      setTimeout(() => setCount(count - 1), 1000)
-    } else {
-      Router.push('https://clubs.triamudom.ac.th')
-    }
-  }, [count])
+    React.useEffect(() => {
+        if (count > 0) {
+            setTimeout(() => setCount(count - 1), 1000)
+        } else {
+            Router.push('https://clubs.triamudom.ac.th')
+        }
+    }, [count])
 
-  return (
-    <div className="mt-4 text-gray-300 text-5s font-display">
-      Redirect to{' '}
-      <a
-        className="hover:text-blue-300 hover:underline"
-        href="https://clubs.triamudom.ac.th"
-      >
-        main site
-      </a>{' '}
-      in {count}
-    </div>
-  )
+    return (
+        <div className="mt-4 text-gray-300 text-5s font-display">
+            Redirect to{' '}
+            <a
+                className="hover:text-blue-300 hover:underline"
+                href="https://clubs.triamudom.ac.th"
+            >
+                main site
+            </a>{' '}
+            in {count}
+        </div>
+    )
 }
 
 export default Timer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
+import React from "react";
 import Timer from '../components/Timer'
+import Ouwae from '../components/Vectors';
 
 const Index = () => (
-  <div className="flex flex-col items-center justify-center min-h-screen">
-    <div className="text-6xl font-bold font-display">
-      TRIAM UDOM OPENHOUSE 2021
+    <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="mb-5">
+            <Ouwae width={163} height={117}/>
+        </div>
+        <div className="text-6xl font-bold font-display">
+            TRIAM UDOM OPENHOUSE 2021
+        </div>
+        <div className="mt-4 text-6xl font-display">เตรียมอุดมโอเพ่นเฮาส์ ๒๐๒๑</div>
+        <Timer/>
     </div>
-    <div className="mt-4 text-6xl font-display">เตรียมอุดมโอเพ่นเฮาส์ ๒๐๒๑</div>
-    <Timer />
-  </div>
 )
 
 export default Index
