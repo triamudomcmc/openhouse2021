42967951638c5669a94e7ee86d770eaeccde3fc6
Peter <peterphongpak@gmail.com>
2021-01-12 17:15:04 +0700

    Refactor svgs and fix circle svg

--- src/components/index/Highlights/index.tsx
+++ src/components/index/Highlights/index.tsx
@@ -2,12 +2,11 @@ import React from 'react'
 import cn from 'classnames'
 
 import s from './Highlights.module.css'
-import { Live } from './LiveSVG'
-import { Clip } from './ClipSVG'
+import {Live, Clip, CircleYellow} from './SVGs'
 
 export const Highlights = () => (
   <div>
-    <div className="flex flex-row mt-20 md:mt-28 h-873 font-display">
+    <div className="flex flex-row mt-20 md:mt-28 h-96 md:h-873 font-display">
       <div className="relative w-full">
         <div className={cn('rounded-full absolute', s.circle1Gradient)} />
         <div className="absolute top-28 left-12 md:top-80 md:left-40">
@@ -30,21 +29,21 @@ export const Highlights = () => (
         </div>
       </div>
     </div>
-    <div className="flex flex-row -mt-32 h-873 font-display">
-      <div className="absolute flex w-full pl-8 text-left md:relative pt-32 md:text-center md:h-full">
+    <div className="flex flex-row -mt-36 md:-mt-32 h-96 md:h-873 font-display">
+      <div className="flex w-full pl-8 text-left relative pt-2 md:pt-32 md:text-center md:h-full">
         <div className="w-full">
           <div className="inline-block text-left">
-            <div className="w-64 pt-2 text-base font-normal text-gray-500 md:text-5xl md:leading-normal md:pt-2 md:w-36rem">
+            <div className="w-40 pt-4 text-base font-normal text-gray-500 md:text-5xl md:leading-normal md:pt-2 md:w-36rem">
               <p>คลิป/บทความที่น่าสนใจ จากรุ่นพี่เตรียมอุดมฯ</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="relative w-full">
-        <div className="">
-          <div className={cn('rounded-full absolute', s.circle2Gradient)} />
+      <div className="relative flex justify-end w-full">
+        <div className="absolute">
+          <CircleYellow />
         </div>
-        <div className="absolute top-28 right-12 md:top-80 md:right-40">
+        <div className="absolute top-20 right-12 md:top-80 md:right-40">
           <Clip />
         </div>
       </div>
