d03d1b6cf1a5563660d60c75a6df480183d19648
Possawat Suksai <markkung08@gmail.com>
2021-01-12 22:25:53 +0700

    Change fontDisplay to optional

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,7 +36,7 @@ module.exports = {
             fontWeight: '100 900',
             fontStyle: 'normal',
             fontNamedInstance: 'Regular',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'url("/assets/fonts/Inter-roman.var.woff2?3.13") format("woff2")',
           },
@@ -47,7 +47,7 @@ module.exports = {
             fontWeight: '100 900',
             fontStyle: 'italic',
             fontNamedInstance: 'Italic',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'url("/assets/fonts/Inter-italic.var.woff2?3.13") format("woff2")',
           },
@@ -57,7 +57,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '400',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai"), local("IBMPlexSansThai"), url("/assets/fonts/IBMPlexSansThai-Regular.woff2") format("woff2")',
           },
@@ -67,7 +67,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '450',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai Text"), local("IBMPlexSansThai-Text"), url("/assets/fonts/IBMPlexSansThai-Text.woff2") format("woff2")',
           },
@@ -77,7 +77,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '500',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai Medium"), local("IBMPlexSansThai-Medium"), url("/assets/fonts/IBMPlexSansThai-Medium.woff2") format("woff2")',
           },
@@ -87,7 +87,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '600',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai SemiBold"), local("IBMPlexSansThai-SemiBold"), url("/assets/fonts/IBMPlexSansThai-SemiBold.woff2") format("woff2")',
           },
@@ -97,7 +97,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '700',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai Bold"), local("IBMPlexSansThai-Bold"), url("/assets/fonts/IBMPlexSansThai-Bold.woff2") format("woff2")',
           },
@@ -107,7 +107,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '100',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai Thin"), local("IBMPlexSansThai-Thin"), url("/assets/fonts/IBMPlexSansThai-Thin.woff2") format("woff2")',
           },
@@ -117,7 +117,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '200',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai ExtraLight"), local("IBMPlexSansThai-ExtraLight"), url("/assets/fonts/IBMPlexSansThai-ExtraLight.woff2") format("woff2")',
           },
@@ -127,7 +127,7 @@ module.exports = {
             fontFamily: 'IBM Plex Sans Thai',
             fontStyle: 'normal',
             fontWeight: '300',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src:
               'local("IBM Plex Sans Thai Light"), local("IBMPlexSansThai-Light"), url("/assets/fonts/IBMPlexSansThai-Light.woff2") format("woff2")',
           },
@@ -135,7 +135,7 @@ module.exports = {
         {
           '@font-face': {
             fontFamily: 'Sukhumvit Set',
-            fontDisplay: 'swap',
+            fontDisplay: 'optional',
             src: "local('Sukhumvit Set')",
             unicodeRange: 'U+0E01-0E5B, U+200C-200D, U+25CC',
           },
