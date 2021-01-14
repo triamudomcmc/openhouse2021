deb5524f5f7115b85070d6f551c53fb086f1e818
Possawat Suksai <markkung08@gmail.com>
2021-01-14 16:00:52 +0700

    Fix timezone config

--- src/utils/getTimestamp.ts
+++ src/utils/getTimestamp.ts
@@ -1,10 +1,11 @@
-import { format } from 'date-fns-tz'
+import { format, utcToZonedTime } from 'date-fns-tz'
 
 export const getTimestamp = (timestamp) => {
   const second = timestamp.seconds ? timestamp.seconds : timestamp._seconds
   const date = new Date(second * 1000)
-  const dd = format(date, 'dd', { timeZone: 'Asia/Bangkok' })
-  const tt = format(date, 'HH:mm', { timeZone: 'Asia/Bangkok' })
+  const bkkDate = utcToZonedTime(date, 'Asia/Bangkok')
+  const dd = format(bkkDate, 'dd', { timeZone: 'Asia/Bangkok' })
+  const tt = format(bkkDate, 'HH:mm', { timeZone: 'Asia/Bangkok' })
   return {
     date: parseInt(dd),
     time: tt,
