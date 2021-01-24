49c8667d255a18424791ece5a79a44f97da585e6
NakZaa <nak11494@gmail.com>
2021-01-24 16:40:03 +0700

    Add clubs page

--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 
 import { Layout } from 'components/common/Layout'
 import Tabs from 'components/article/Tabs'
