e2bfd05a3240594b49c59ed601b6f65fb2ae3873
Peter <peterphongpak@gmail.com>
2021-02-06 00:06:47 +0700

    Implement scroll bar

--- src/components/index/Videos.tsx
+++ src/components/index/Videos.tsx
@@ -6,12 +6,15 @@ import { ContentCard } from '../common/Card/ContentCard'
 const Videos = () => {
   return (
     <div className="w-4/5 md:w-7/12">
-      <h1 className="font-black text-center text-blue-400 mt-16 md:mt-48 text-3xl md:text-7xl mb-10">
-        วีดีโอ
-      </h1>
+      <div className="flex flex-row justify-between md:justify-center mt-16 mb-10 items-end">
+        <h1 className="text-3xl font-black text-center text-blue-400 md:mt-48 md:text-7xl">
+          วีดีโอ
+        </h1>
+        <h1 className="block md:hidden font-bold text-lg text-gray-400">ดูทั้งหมด</h1>
+      </div>
       <div className="flex flex-col items-center justify-center py-4 space-y-4">
-        <div className="flex flex-col max-w-full space-y-4 md:space-y-0 md:space-x-4 md:flex-row">
-          <ContentCard src="/assets/nok.png" duration="12.30">
+        <div className="flex max-w-full pb-6 space-y-4 overflow-x-scroll md:space-y-0 md:space-x-4 flex-row">
+          <ContentCard src="/assets/nok.png" duration="12.30" className="md:w-1/3">
             <ContentCard.Desc>
               เปิดพิกัด ต้นต้อยติ่งในโรงเรียน ! อาวุธสง ครามของต้องห้ามประจำโรงเรียนเตรียมอุดมศึกษา
             </ContentCard.Desc>
@@ -30,7 +33,7 @@ const Videos = () => {
             <ContentCard.Author>ไอร่’ ตะวัน นน’ คนไม่มี คัยย.</ContentCard.Author>
           </ContentCard>
         </div>
-        <div className="flex flex-col max-w-full space-y-4 md:space-y-0 md:space-x-4 md:flex-row">
+        <div className="flex max-w-full pb-6 space-y-4 overflow-x-scroll md:space-y-0 md:space-x-4 flex-row">
           <ContentCard src="/assets/nok.png" duration="12.30">
             <ContentCard.Desc>
               เปิดพิกัด ต้นต้อยติ่งในโรงเรียน ! อาวุธสง ครามของต้องห้ามประจำโรงเรียนเตรียมอุดมศึกษา
