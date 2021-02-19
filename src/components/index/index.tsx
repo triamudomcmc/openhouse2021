72b70c1199ac7c6b9a46081952e3600fb9cb8dc8
Peter <peterphongpak@gmail.com>
2021-02-19 23:25:36 +0700

    Re design live section in index page

--- src/components/index/index.tsx
+++ src/components/index/index.tsx
@@ -22,6 +22,7 @@ import { Facebook } from '../common/Logo/Facebook'
 import { Email } from '../common/Logo/Email'
 import Router from 'next/router'
 import InApp from 'detect-inapp'
+import { VideoVector } from '../../assets/vectors/index/VideoVector'
 
 export const Index = ({ stream, schedule, contents, videos }) => {
   const [blocked, setBlocked] = useState(false)
@@ -99,9 +100,19 @@ export const Index = ({ stream, schedule, contents, videos }) => {
                 whileHover={{ scale: 1.04 }}
                 whileTap={{ scale: 0.96 }}
                 type="button"
-                className="inline-flex items-center px-10 py-2 text-base font-bold text-white text-purple-200 bg-white border border-transparent rounded-full md:py-4 w-max md:text-3xl font-display focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-200"
+                className="inline-flex hidden sm:inline-flex items-center px-12 shadow-sm py-2 text-base font-black text-white text-purple-400 bg-white border border-transparent rounded-full md:py-4 w-max md:text-3xl font-display focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-200"
               >
-                เข้าร่วมงาน
+                เข้าสู่ระบบ
+              </motion.button>
+            </Link>
+            <Link href="/register">
+              <motion.button
+                whileHover={{ scale: 1.04 }}
+                whileTap={{ scale: 0.96 }}
+                type="button"
+                className="inline-flex block sm:hidden items-center px-24 py-2 shadow-sm text-base font-black text-white text-purple-400 bg-white border border-transparent rounded-xl md:py-4 w-max md:text-3xl font-display focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-pink-200"
+              >
+                เข้าสู่ระบบ
               </motion.button>
             </Link>
           </div>
@@ -124,90 +135,21 @@ export const Index = ({ stream, schedule, contents, videos }) => {
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.95 }}
           onClick={redirectToStage}
-          className="w-4/5 px-5 py-5 mb-3 bg-white shadow-lg md:w-7/12 rounded-3xl md:px-10 smd:py-8"
+          className="w-4/5 px-5 py-5 mb-3 bg-white shadow-lg md:w-7/12 rounded-xl sm:rounded-2xl md:rounded-3xl md:px-10 smd:py-8"
         >
-          <div className="mb-2 ml-2 font-medium lg:mb-8">
-            <div className="flex items-center text-xs md:text-2xl">
-              <span className="px-2 text-xs font-semibold text-white bg-gray-400 md:text-2xl">
-                OFFLINE
-              </span>
-              <span className="ml-2 md:ml-4">{liveContent.title}</span>
-            </div>
-            <div className="mt-1 text-xs text-gray-300 min-w-min md:text-2xl">
-              {liveContent.club} | วันที่ 13 กุมภาพันธ์ 2021
-            </div>
-          </div>
-          <div className="frame-height-mobile md:frame-height-desktop">
-            {!loading && user ? (
-              <iframe
-                src={`${updatedStream.stream}`}
-                frameBorder="0"
-                allowFullScreen
-                scrolling="no"
-                height="100%"
-                width="100%"
-              ></iframe>
-            ) : (
-              <div className="flex flex-col h-full justify-center">
-                {!blocked ? (
-                  <div className="flex flex-col items-center justify-center flex-1 px-4">
-                    <h1 className="text-sm md:text-xl font-bold text-center text-gray-500 lg:text-3xl">
-                      โปรดลงทะเบียน
-                    </h1>
-                    <h1 className="text-sm md:text-xl font-bold text-center text-gray-500 lg:text-3xl">
-                      เพื่อเข้าชมการถ่ายทอดสด
-                    </h1>
-                    <div className="flex flex-col items-center justify-center mt-2 space-y-1 lg:space-y-4 lg:mt-6">
-                      <button
-                        type="button"
-                        className="inline-flex items-center justify-center w-full px-5 py-1 md:py-2 text-xxs font-medium text-center text-gray-600 bg-white border border-transparent rounded-full shadow-md hover:bg-gray-100 md:px-10 lg:text-xl focus:outline-none"
-                        onClick={() => signinWithGoogle('/stage')}
-                      >
-                        <Google className="w-5 h-5 mr-4" />
-                        Sign in with Google
-                      </button>
-                      <button
-                        type="button"
-                        className="inline-flex items-center justify-center w-full px-5 py-1 md:py-2 text-xxs font-medium text-center text-gray-600 bg-white border border-transparent rounded-full shadow-md hover:bg-gray-100 md:px-10 lg:text-xl focus:outline-none"
-                        onClick={() => signinWithFacebook('/stage')}
-                      >
-                        <Facebook className="w-5 h-5 mr-4" />
-                        Sign in with Facebook
-                      </button>
-                      <Link href="/signup">
-                        <button
-                          type="button"
-                          className="inline-flex items-center justify-center w-full px-5 py-1 md:py-2 text-xxs font-medium text-center text-gray-600 bg-white border border-transparent rounded-full shadow-md hover:bg-gray-100 md:px-10 lg:text-xl focus:outline-none"
-                        >
-                          <Email className="w-5 h-5 mr-4" />
-                          Sign in with email
-                        </button>
-                      </Link>
-                    </div>
-                  </div>
-                ) : (
-                  <div className="flex flex-col items-center justify-center flex-1 px-4">
-                    <h1 className="text-sm md:text-xl font-bold text-center text-gray-500 lg:text-3xl">
-                      โปรดลงทะเบียน
-                    </h1>
-                    <h1 className="text-sm md:text-xl font-bold text-center text-gray-500 lg:text-3xl">
-                      เพื่อเข้าชมการถ่ายทอดสด
-                    </h1>
-                    <div className="flex flex-col items-center justify-center mt-2 space-y-1 lg:space-y-4 lg:mt-6">
-                      <Link href="/signup">
-                        <button
-                          type="button"
-                          className="inline-flex items-center justify-center w-full px-5 py-1 md:py-2 text-xxs font-medium text-center text-gray-600 bg-white border border-transparent rounded-full shadow-md hover:bg-gray-100 md:px-10 lg:text-xl focus:outline-none"
-                        >
-                          <Email className="w-5 h-5 mr-4" />
-                          Sign in with email
-                        </button>
-                      </Link>
-                    </div>
-                  </div>
-                )}
-              </div>
-            )}
+          <div className="flex flex-col justify-center items-center py-0 md:py-6 lg:py-14">
+            <VideoVector
+              style={{ height: '10vw', width: '15vw', minWidth: '100px', minHeight: '80px' }}
+            />
+            <h1 style={{ fontSize: 'calc(5px + 3.5vw)' }} className="text-pink-300 font-black">
+              รายการสด
+            </h1>
+            <h1
+              style={{ fontSize: 'calc(5px + 2vw)', lineHeight: 'calc(5px + 1vw)' }}
+              className="text-gray-400 font-black"
+            >
+              ย้อนหลัง
+            </h1>
           </div>
         </motion.div>
         <Link href="/articles/admission">
@@ -215,7 +157,7 @@ export const Index = ({ stream, schedule, contents, videos }) => {
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.95 }}
             style={{ paddingTop: '2.4vw', paddingBottom: '2.4vw' }}
-            className="flex flex-row justify-center w-4/5 px-4 mb-3 bg-white shadow-lg md:w-7/12 rounded-3xl md:px-12"
+            className="flex flex-row justify-center w-4/5 px-4 mb-3 bg-white shadow-lg md:w-7/12 rounded-xl sm:rounded-2xl md:rounded-3xl md:px-12"
           >
             <div className="flex flex-col items-center justify-center mx-auto">
               <h1
@@ -288,7 +230,7 @@ export const Index = ({ stream, schedule, contents, videos }) => {
                   style={{ width: '36%', height: '36%' }}
                   className="my-1 h-7 sm:h-10 md:h-24"
                 />
-                การเดินทาง
+                <div style={{ marginTop: '0.4vw' }}>การเดินทาง</div>
               </div>
             </Link>
           </div>
