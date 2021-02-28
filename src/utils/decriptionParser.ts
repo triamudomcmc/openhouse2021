52052065cbf15f4e11d5ae302995b95b521d3de0
Peter <peterphongpak@gmail.com>
2021-03-01 03:48:07 +0700

    Fix ig typo

--- src/utils/decriptionParser.ts
+++ src/utils/decriptionParser.ts
@@ -4,7 +4,6 @@ export const parseDecription = async (rawHTML: string): Promise<string> => {
   const igcount = (rawHTML.match(/@/g) || []).length
 
   for (let i = 0; i < igcount; i++) {
-    console.log(html.split('@', 2)[1].split('<', 2)[0].trim())
     const username = html.split('@', 2)[1].split('<', 2)[0].trim()
     const identifier = `@${username}`
     const linkElement = `<a class="hover:text-pink-200" target="_blank" href="https://www.instagram.com/${username}/">${identifier}</a>`
